e2c36cb079ea65d7454a5477221e11ec6a3a0142
starkensin <starkensin@gmail.com>
2022-03-06 20:58:21 +0900

    initialize backend project #2

--- backend/src/main/java/com/starkensin/giftbasket/GiftbasketApplication.java
+++ backend/src/main/java/com/starkensin/giftbasket/GiftbasketApplication.java
@@ -1,7 +1,11 @@
 package com.starkensin.giftbasket;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class GiftbasketApplication {
@@ -10,4 +14,12 @@ public class GiftbasketApplication {
 		SpringApplication.run(GiftbasketApplication.class, args);
 	}
 
+
+	@Value("${spring.config.activate.on-profile}")
+	private String app;
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void init() {
+		System.out.println("$==========================spring.config.activate.on-profile: "+app+"==========================");
+	}
 }
