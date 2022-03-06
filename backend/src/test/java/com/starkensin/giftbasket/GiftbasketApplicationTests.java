a0e9bc96edea11c69efae4042663cae4bf7c8e2b
starkensin <starkensin@gmail.com>
2022-03-06 15:26:46 +0900

    init

---
+++ backend/src/test/java/com/starkensin/giftbasket/GiftbasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starkensin.giftbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftbasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
